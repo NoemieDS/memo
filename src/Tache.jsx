98a8305d8034d4875f8e070b216b67e265193b63
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-03-07 17:15:17 -0500

    Responsive

--- src/Tache.jsx
+++ src/Tache.jsx
@@ -1,10 +1,17 @@
-import './Tache.scss';
+import "./Tache.scss";
 
 export default function Tache() {
-
-  return(
+  return (
     <article className="Tache">
-      <h4 className='tache-titre'>Avancer mon TP 1</h4>
-      </article>
-  ) 
+      <div className="tache-haut">
+        <p className="tache-date">Date</p>
+        <input type="checkbox" name="tache-completer" id="tache-completer" />
+      </div>
+      <h4 className="une-tache">Avancer mon TP 1 et intégrer le formulaire aujourd'hui. </h4>
+      <div className="tache-bas">
+        <input type="checkbox" name="tache-annuler" id="tache-annuler" />
+        <input type="checkbox" name="tache-modifier" id="tache-modifier" />
+      </div>
+    </article>
+  );
 }
