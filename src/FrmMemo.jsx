2534b7d59afd018b9bd22c50fdaef2e4a2eb9c22
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-03-08 19:40:03 -0500

    Fini MUI! form et button html, le formulaire se vide, on n'ajoute pas de mémo au tableau des mémos si la chaine est vide

--- src/FrmMemo.jsx
+++ src/FrmMemo.jsx
@@ -4,41 +4,59 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useState } from "react";
 
-export default function FrmMemo({memos, setMemos}) {
+export default function FrmMemo({ memos, setMemos }) {
   const [texte, setTexte] = useState("");
 
-  function ajouterMemo() {
-    let timestamp = new Date().getTime();
-    let id = crypto.randomUUID();
-    let memo = {
-      id: id,
-      date: timestamp,
-      texte: texte,
-      complete: false,
-    };
-    
-    setMemos([
-      ...memos,
-      {
+  function ajouterMemo(evt) {
+       
+      let timestamp = new Date().getTime();
+      let id = crypto.randomUUID();
+      let memo = {
         id: id,
         date: timestamp,
         texte: texte,
         complete: false,
-      }
-    ])
+      };
+
+      if (memo.texte.length !== 0) {
+      setMemos([
+        ...memos,
+        {
+          id: id,
+          date: timestamp,
+          texte: texte,
+          complete: false,
+        },
+      ]);
+    }
   }
-console.log(memos.texte)
+
   return (
     <section className="FrmMemo">
-      <TextField
+      <form action="" onSubmit={ajouterMemo} id="frmMemo">
+        <input
+          type="text"
+          id="input-memo"
+          name="input-memo"
+          autoFocus
+          autoComplete="off"
+          placeholder="Je veux me rappeller de..."
+          onChange={(evt) => setTexte(evt.target.value)}
+        ></input>
+        <button type="submit" form="frmMemo" value="Submit">
+          Ajouter
+        </button>
+      </form>
+
+      {/*       <TextField
         id="filled-basic"
         label="Aujouter un mémo"
         variant="filled"
         onChange={(evt) => setTexte(evt.target.value)}
-      />
-      <Button variant="contained" color="success" onClick={ajouterMemo}>
+      /> */}
+      {/*  <Button variant="contained" color="success" onClick={ajouterMemo}>
         Ajouter
-      </Button>
+      </Button> */}
       {/*    {console.log('test')} */}
     </section>
   );
